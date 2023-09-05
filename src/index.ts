72e8b8da9ddcec5b8687720736b79dc25e39b080
Alice Hincu <alice.hincu@tora.com>
2023-09-06 02:15:14 +0300

    changed cors for deploy to get the domain2

--- src/index.ts
+++ src/index.ts
@@ -27,7 +27,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["postureflawless.com", "localhost:3000"],
+    origin: ["https://postureflawless.com", "http://localhost:3000"],
     credentials: true,
   })
 );
@@ -39,7 +39,7 @@ app.use(bodyParser.json());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["postureflawless.com", "localhost:3000"],
+    origin: ["https://postureflawless.com", "http://localhost:3000"],
     methods: ["GET", "POST"],
   },
 });
